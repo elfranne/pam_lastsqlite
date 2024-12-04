8d14fee8ecce2fa1359b6ccff1ef0b24d752ede1
Elfranne <861038+elfranne@users.noreply.github.com>
2024-12-04 13:55:03 +0100

    wip

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# pam_lastsqlite
-pam_lastsqlite is an alternative to pam_lastlog
+# pam_lastsqlite.py
+While trying to implement pam_lastlog for a vsftpd server I ran into issues with the module trying to lookup the users in the OS, but those users are not present as system users and are being authenticated with pam_pwdfile.
+The good thing is that pam_lastlog is being deprecated for pam_lastlog2, but that version has not been pulled yet into Debian/Ubuntu LTS repos yet (as of december 2024).
+
+# setup
+Arch has a very good guide on how to [setup vsftpd](https://wiki.archlinux.org/title/Very_Secure_FTP_Daemon)
+
+# pam
+If you follow the guide above you should edit, and add a line `/etc/pam.d/vsftpd`:
+```
+session optional pam_exec.so /usr/local/bin/pam_lastsqlite.py --file /path/to/lastlog.db
+auth    required pam_pwdfile.so pwdfile /etc/vsftpd/vsftp-users
+account required pam_permit.so
+```
+
+# useful commands
---
+++ pam_lastsqlite.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python3
+
+import sqlite3
+import argparse
+import time
+import os
+import socket
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--create', action='store_true',
+                    help='Create database file and initialize')
+parser.add_argument('--file', dest='file', type=str,
+                    help='Location of the database file')
+parser.add_argument('--load', dest='load', type=str,
+                    help='Load accounts from file')
+args = parser.parse_args()
+
+
+def create_db(db):
+    try:
+        con = sqlite3.connect(db)
+        cursor = con.cursor()
+        cursor.execute(
+            "CREATE TABLE lastlog(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, user TEXT, date INTEGER)")
+        cursor.close()
+        con.close()
+        print("Database initialized")
+    except sqlite3.Error as e:
+        print(f"create_db: {e}")
+
+
+def update_db(db):
+    try:
+        conn = sqlite3.connect(db)
+        cursor = conn.cursor()
+        current_time = int(time.time())
+        user = os.environ.get('PAM_USER')
+
+        cursor.execute(
+            "SELECT * FROM lastlog WHERE user=?", (user,))
+        existing_user = cursor.fetchone()
+        if existing_user:
+            cursor.execute(
+                "UPDATE lastlog SET date=? WHERE user=?", (current_time,user,))
+        else:
+            cursor.execute(
+                "INSERT INTO lastlog (user, date) VALUES (?, ?)", (user,current_time,))
+        conn.commit()
+        # print(f"User '{params[0]}' {'updated' if existing_user else 'inserted'} successfully.")
+        cursor.close()
+        conn.close()
+    except sqlite3.Error as e:
+        print(f"update_db: {e}")
+
+
+def load_db(db, users_file):
+    try:
+        conn = sqlite3.connect(db)
+        cursor = conn.cursor()
+        import csv
+        with open(users_file) as csvfile:
+            list = csv.reader(csvfile, delimiter=':')
+            for row in list:
+                print(row[0])
+                # data = (
+                #     row[0],
+                #     int(time.time()),
+                # )
+                # cursor.execute( "INSERT INTO lastlog (user, date) VALUES (?, ?)", data )
+        conn.commit()
+        cursor.close()
+        conn.close()
+    except sqlite3.Error as e:
+        print(f"load_db: {e}")
+
+
+# MAIN
+if args.create:
+    create_db(args.file)
+    exit(0)
+elif args.load:
+    load_db(args.file, args.load)
+    exit(0)
+else:
+    update_db(args.file)
+    exit(0)
